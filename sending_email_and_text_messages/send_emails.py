860c85ebdbb7a981c37b042027e7f4054585d326
Wei <weinini007@gmail.com>
2018-02-07 00:32:57 +0100

    Add send_emails.py

---
+++ sending_email_and_text_messages/send_emails.py
@@ -0,0 +1,9 @@
+import smtplib
+
+smtpObj = smtplib.SMTP('smtp.example.com', 587)
+smtpObj.ehlo()
+smtpObj.starttls()
+smtpObj.login('bob@example.com','MY_SECRET_PASSWORD')
+smtpObj.sendmail('bob@example.com', 'alice@example.com', 'Subject: So long.\n\
+                Dear Alice, so long and thanks for all the fish. Sincerely, Bob')
+smtpObj.quit()
